faaef76038e058888171b5286ed3be911129f8c1
kaziabulhasib <hasibkazi420@gmail.com>
2024-10-16 23:33:14 +0530

    Patient form set  up

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className='flex h-screen max-h-screen'>
+      {/* to do: Otp verification */}
       <section className='container remove-scrollbar my-auto'>
         <div className='sub-container max-w-[496px]'>
           <Image
